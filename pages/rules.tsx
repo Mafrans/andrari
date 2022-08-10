33c711309494e7a711b337ac0e09c55119651896
Malte <malte.kluft@gmail.com>
2022-08-10 13:20:19 +0200

    feat: add rules page

---
+++ pages/rules.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Rules: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>rules</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Rules;
