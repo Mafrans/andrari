5fc82f9b39e1d481f442dcc87eafb72646b5b449
Malte <malte.kluft@gmail.com>
2022-08-11 13:31:06 +0200

    feat(blocks): add text variation to Text block

--- components/blocks/Text/Text.tsx
+++ components/blocks/Text/Text.tsx
@@ -1,7 +1,8 @@
 import ReactMarkdown from "react-markdown";
 import { Component } from "../../../types/components";
 import { TextProps } from "./types";
+import styles from "./styles.module.css";
 
 export const Text: Component<TextProps> = ({ Content }) => {
-  return <ReactMarkdown>{Content}</ReactMarkdown>;
+  return <ReactMarkdown className={styles.text}>{Content}</ReactMarkdown>;
 };
